cd90d1ecdeaae4576797271ae957029c755131d2
rockerrishabh <rishabh.6542@gmail.com>
2022-07-26 16:36:46 +0530

    fourth

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
-  ],
+  plugins: [solidPlugin(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     port: 3000,
   },
