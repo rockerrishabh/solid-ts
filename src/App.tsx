5f2f4effa27a7e37716d64bab827be881949d624
rockerrishabh <rishabh.6542@gmail.com>
2022-07-26 16:19:50 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class="">
+      <header class="">
+        <h1 class="">Blog</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
