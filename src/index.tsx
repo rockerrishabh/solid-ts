a05d60429f9a6fdf6758b2222c2ece20a523cbe8
rockerrishabh <rishabh.6542@gmail.com>
2022-07-28 14:19:25 +0530

    tenth

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,26 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 
+const APP_NAME = "Solid Ts";
+const APP_DESCRIPTION = "Solid Ts";
+
 render(
   () => (
     <>
       <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>My Awesome App</title>
-        <meta name="description" content="My Awesome App description" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
