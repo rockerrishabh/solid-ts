9bb5333e94536629455d40b726a783929af723b6
rockerrishabh <rishabh.6542@gmail.com>
2022-07-28 14:12:13 +0530

    nineth

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+  () => (
+    <>
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>My Awesome App</title>
+        <meta name="description" content="My Awesome App description" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+      <App />
+    </>
+  ),
+  document.getElementById("root") as HTMLElement
+);
